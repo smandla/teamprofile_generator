20f909771ec477b8c3c1a7aec23557e2a6523d01
smandla <35283559+smandla@users.noreply.github.com>
2022-07-18 19:36:02 -0700

    checks employee type

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const arr = [];
 function init() {
   managerQuestions();
 }
@@ -16,7 +17,34 @@ function managerQuestions() {
     },
   ];
   inquirer.prompt(questions).then((answers) => {
-    console.log(answers);
+    // console.log(answers);
+    arr.push(answers);
+    console.log(arr);
+    addEmployee();
+  });
+}
+function addEmployee() {
+  const employeeTypeQuestion = [
+    {
+      name: "employeeType",
+      message: "Which type of team member would you like to add?",
+      type: "list",
+      choices: [
+        "Engineer",
+        "Intern",
+        "I don't want to add any more team members.",
+      ],
+    },
+  ];
+  inquirer.prompt(employeeTypeQuestion).then((answers) => {
+    // console.log(answers);
+    if (answers.employeeType === "Engineer") {
+      console.log("Engineer");
+    } else if (answers.employeeType === "Intern") {
+      console.log("Intern");
+    } else {
+      console.log("NOne");
+    }
   });
 }
 init();
